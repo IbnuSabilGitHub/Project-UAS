c1cdd972f74c9f23861d7dd3b20021c45d58707f
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-11-25 20:12:01 +0800

    refactor: UI applies for leave

--- app/Views/leave/form.php
+++ app/Views/leave/form.php
@@ -33,25 +33,42 @@
                 </select>
             </div>
 
-            <!-- Tanggal Mulai -->
+            <!-- Tanggal Range -->
             <div class="mb-4">
-                <label for="start_date" class="block text-sm font-medium text-heading mb-2">
-                    Tanggal Mulai <span class="text-red-500">*</span>
+                <label class="block text-sm font-medium text-heading mb-2 ">
+                    Pilih Rentang Tanggal
                 </label>
-                <input type="date" name="start_date" id="start_date" required
-                    min="<?= date('Y-m-d') ?>"
-                    class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow">
+                <div id="date-range-picker" date-rangepicker class="flex items-center gap-4 w-full">
+
+                    <!-- start date -->
+                    <div class="relative flex-1">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
+                            </svg>
+                        </div>
+                        <input id="start_date" name="start_date" type="text"
+                            class="block w-full ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
+                            placeholder="Select date start">
+                    </div>
+
+                    <span class="text-body">to</span>
+
+                    <!-- end date -->
+                    <div class="relative flex-1">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
+                            </svg>
+                        </div>
+                        <input id="end_date" name="end_date" type="text"
+                            class="block w-full ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
+                            placeholder="Select date end">
+                    </div>
+                </div>
+
             </div>
 
-            <!-- Tanggal Selesai -->
-            <div class="mb-4">
-                <label for="end_date" class="block text-sm font-medium text-heading mb-2">
-                    Tanggal Selesai <span class="text-red-500">*</span>
-                </label>
-                <input type="date" name="end_date" id="end_date" required
-                    min="<?= date('Y-m-d') ?>"
-                    class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow">
-            </div>
 
             <!-- Total Hari (auto calculate) -->
             <div class="mb-4">
